b38b68283007161139df08eb51000c7cda637907
KarsCode <kartiksubbaram18@gmail.com>
2023-11-01 11:15:03 +0530

    Commit Made

--- src/LoginSceneController.java
+++ src/LoginSceneController.java
@@ -13,13 +13,32 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class LoginSceneController implements Initializable {
+class user{
+    String name;
+    int uid;
+
+    user(String name, int uid){
+        this.name = name;
+        this.uid = uid;
+    }
+}
+
+public class LoginSceneController extends user implements Initializable{
 
     @FXML
     private Button LoginBtn;
     @FXML
     private TextField loginCred;
-    //private Button LoginCloseBtn;
+    //private Button LoginCloseBtn
+    public LoginSceneController() {
+        super("", 0); // Invoke user's constructor with default or placeholder values
+    }
+
+
+    LoginSceneController(String name, int uid)
+    {
+        super(name,uid);
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -49,8 +68,63 @@ public class LoginSceneController implements Initializable {
     @FXML
     public void LoginFunc(ActionEvent event) {
          String userInput = loginCred.getText(); // Retrieve the text from the TextField
-         System.out.println("User input: " + userInput); // Print the input (you can handle it as needed)
+         System.out.println("User input: " + userInput);
+
+            user u[]= new user[]{
+            new user("dog", 123),
+            new user("cat", 356),
+            new user("cow", 678),
+        };
+         
+        int regid;
         
+            try{
+                regid = Integer.parseInt(userInput);
+                credentials(regid, u);
+            }catch(NumberFormatException e){
+                credentials(userInput,u);
+        }// Print the input (you can handle it as needed)
+
+        try {
+            Parent loader = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+            Scene LoginPageScene = new Scene(loader);
+            Stage appStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            
+            appStage.setScene(LoginPageScene);
+            appStage.show();
+        } catch (IOException e) {
+            e.printStackTrace(); // Handle the exception as needed
+        }
+        
+    }
+
+        public static <T> void credentials(T id, user u[])  {
+        System.out.println("Test");
+        int in = -1;
+        if (id instanceof Integer) {
+            int regid = (Integer) id;
+            for(int i=0;i<3;i++){
+                if (regid == u[i].uid){
+                    in = i;
+                    break;
+                }
+            }
+        }
+         else if (id instanceof String) {
+            String name = (String) id;
+        
+            for(int i=0;i<3;i++){
+                if(u[i].name.toLowerCase().contains(name.toLowerCase())){
+                    in = i;
+                    break;
+                }
+            }
+        } 
+        else {
+            System.out.println("Invalid input type");
+        }
+        if(in != -1) 
+        System.out.println("Welcome back, "+u[in].name +"!");
     }
 }
 
